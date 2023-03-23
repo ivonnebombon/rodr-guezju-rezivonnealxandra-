13710f19eaa9cf0e59ed4ec9074cda3ce80af6a1
ivonnebombon <126205750+ivonnebombon@users.noreply.github.com>
2023-03-22 18:26:39 -0600

    Add files via upload

---
+++ M1/S3/p2/Java/VectorIvertido.java
@@ -0,0 +1,12 @@
+mport java.io.*;
+class VectorInvertido
+  {
+   public static void main(String[] args)
+   { 
+    int[] listaNumeros = {37,5,10,3};
+   for( int i= listaNumeros.length-1;4>= 0; i--)
+    
+    System.out.println(listaNumeros[i]);
+ 
+   }
+  }
---
+++ M1/S3/p2/Java/longitud.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class longitud
+ {
+  public static void main(String[] args)
+    {
+     int[] listaNumeros={0,9,4,1,3,7,10};
+     System.out.println(listaNumeros.length);
+    }
+ }
