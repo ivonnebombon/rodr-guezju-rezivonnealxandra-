bd04db9ef29f7578c635d1d2cbe1130adf09ce7a
ivonnebombon <126205750+ivonnebombon@users.noreply.github.com>
2023-04-19 16:40:34 -0600

    Delete M1 directory

--- M1/S3/p2/Java/longitud.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-class longitud
- {
-  public static void main(String[] args)
-    {
-     int[] listaNumeros={0,9,4,1,3,7,10};
-     System.out.println(listaNumeros.length);
-    }
- }
